a3443d750a6941198f1029d1652067f612203856
Woody Lam <woody.lam.cwl@gmail.com>
2022-01-25 00:27:21 +0000

    Embed logger to Motor module

--- Arduino/Modules/Motor.cpp
+++ Arduino/Modules/Motor.cpp
@@ -1,7 +1,12 @@
 #include "Motor.h"
 
+MotorController::MotorController(Logger *logger) {
+    this->logger = logger;
+}
+
 void MotorController::setup() {
     motorShield = Adafruit_MotorShield();
     leftMotor = motorShield.getMotor(LEFT_MOTOR_PORT);
     rightMotor = motorShield.getMotor(RIGHT_MOTOR_PORT);
+    logger->log("Motor controller initialised", LoggerLevel::Info);
 }
--- Arduino/Modules/Motor.h
+++ Arduino/Modules/Motor.h
@@ -4,18 +4,24 @@
 #include <Arduino.h>
 #include <Adafruit_MotorShield.h>
 #include "../Constants.h"
+#include "Logger.h"
 
 class MotorController {
     private:
         Adafruit_MotorShield motorShield;
         Adafruit_DCMotor *leftMotor;
         Adafruit_DCMotor *rightMotor;
+        Logger *logger;
     public:
+        MotorController(Logger *logger);
         void setup();
 };
 
 class ServoController {
+    private:
+        Logger logger;
     public:
+        ServoController(Logger *logger);
         void setup();
 };
 
