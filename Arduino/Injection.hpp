4c436d0d19e87f2f6899fdfdc3882a72c38dad88
Woody Lam <woody.lam.cwl@gmail.com>
2022-02-10 14:40:04 +0000

    Implement new injection model

--- Arduino/Injection.hpp
+++ Arduino/Injection.hpp
@@ -2,19 +2,39 @@
 #define Injection_H
 
 #include "Stages.hpp"
+#include "Transition.hpp"
+
+enum class EnumStage : byte {
+    ForwardLineTracing,
+    Turning,
+    GrabClassifyBlock,
+    ReleaseBlock,
+    ReverseMotion
+};
+
+enum class EnumTransition : byte {
+    Once,
+    Timed,
+    DetectObstruction,
+    DetectLine,
+    DetectCross
+};
 
 class Injection {
     public:
         Injection();
         Logger *logger;
-
-        LineTracing *lineTracing;
-        Turning *turning;
-
-        DetectBlock *detectBlock;
-        DetectCross *detectCross;
-        FinishTurn *finishTurn;
-        TimedTurn *timedTurn;
+        
+        IStage* getNewStage(
+            IStage *currentStage,
+            EnumStage stage,
+            bool turnLeft = true
+        );
+        ITransition* getNewTransition(
+            ITransition *currentTransition,
+            EnumTransition transition,
+            unsigned long suppressTime = 0
+        );
 
     private:
         MotorController *motorController;
@@ -24,6 +44,18 @@ class Injection {
         LineSensor *lineSensor;
         UltrasonicSensor *ultrasonicSensor;
         InfraRed *infraRed;
+
+        ForwardLineTracing* getNewForwardLineTracing();
+        Turning* getNewTurning(bool turnLeft);
+        GrabClassifyBlock* getNewGrabClassifyBlock();
+        ReleaseBlock* getNewReleaseBlock();
+        ReverseMotion* getNewReverseMotion();
+
+        Once* getNewOnce(unsigned long suppressTime = 0);
+        Timed* getNewTimed(unsigned long suppressTime = 0);
+        DetectObstruction* getNewDetectObstruction(unsigned long suppressTime = 0);
+        DetectLine* getNewDetectLine(unsigned long suppressTime = 0);
+        DetectCross* getNewDetectCross(unsigned long suppressTime = 0);
 };
 
 #endif
--- Arduino/InjectionImpl.cpp
+++ Arduino/InjectionImpl.cpp
@@ -1,6 +1,7 @@
 #include "Injection.hpp"
 
-Injection::Injection() {
+Injection::Injection()
+{
     logger = new Logger();
 
     motorController = new MotorController(logger);
@@ -10,84 +11,170 @@ Injection::Injection() {
     lineSensor = new LineSensor(logger);
     ultrasonicSensor = new UltrasonicSensor(logger);
     infraRed = new InfraRed(logger);
+}
 
-    lineTracing = new LineTracing(
+IStage* Injection::getNewStage(
+    IStage *currentStage,
+    EnumStage stage,
+    bool turnLeft = true
+)
+{
+    delete currentStage;
+    IStage* newStage;
+    switch (stage) {
+        case EnumStage::ForwardLineTracing:
+            newStage = getNewForwardLineTracing();
+            break;
+
+        case EnumStage::Turning:
+            newStage = getNewTurning(turnLeft);
+            break;
+
+        case EnumStage::GrabClassifyBlock:
+            newStage = getNewGrabClassifyBlock();
+            break;
+
+        case EnumStage::ReleaseBlock:
+            newStage = getNewReleaseBlock();
+            break;
+
+        case EnumStage::ReverseMotion:
+            newStage = getNewReverseMotion();
+            break;
+
+
+        default:
+            newStage = nullptr;
+            break;
+    }
+
+    return newStage;
+}
+
+ITransition* Injection::getNewTransition(
+    ITransition *currentTransition,
+    EnumTransition transition,
+    unsigned long suppressTime = 0
+)
+{
+    delete currentTransition;
+    ITransition* newTransition;
+
+    switch (transition) {
+        case EnumTransition::Once:
+            newTransition = getNewOnce(suppressTime);
+            break;
+        
+        case EnumTransition::Timed:
+            newTransition = getNewTimed(suppressTime);
+            break;
+
+        case EnumTransition::DetectObstruction:
+            newTransition = getNewDetectObstruction(suppressTime);
+            break;
+
+        case EnumTransition::DetectLine:
+            newTransition = getNewDetectLine(suppressTime);
+            break;
+
+        case EnumTransition::DetectCross:
+            newTransition = getNewDetectCross(suppressTime);
+            break;
+
+        default:
+            newTransition = nullptr;
+            break;
+    }
+
+    return newTransition;
+}
+
+ForwardLineTracing* Injection::getNewForwardLineTracing()
+{
+    return new ForwardLineTracing(
         logger,
         motorController,
-        lineSensor,
-        ledController
-    );
-
-    turning = new Turning(
-        logger,
-        motorController,
-        ledController
-    );
-
-    detectBlock = new DetectBlock(
-        logger,
-        motorController,
-        servoController,
         ledController,
-        infraRed,
+        lineSensor
+    );
+}
+
+Turning* Injection::getNewTurning(bool turnLeft = true)
+{
+    return new Turning(
+        logger,
+        motorController,
+        ledController,
+        turnLeft
+    );
+}
+
+GrabClassifyBlock* Injection::getNewGrabClassifyBlock()
+{
+    return new GrabClassifyBlock(
+        logger,
+        motorController,
+        ledController,
+        servoController,
         ultrasonicSensor
     );
+}
 
-    detectCross = new DetectCross(
-        logger,
-        motorController,
-        servoController,
-        ledController,
-        lineSensor
-    );
-
-    finishTurn = new FinishTurn(
+ReleaseBlock* Injection::getNewReleaseBlock()
+{
+    return new ReleaseBlock(
         logger,
         motorController,
         ledController,
-        lineSensor
+        servoController
     );
+}
 
-    timedTurn = new TimedTurn(
-        logger,
-        motorController,
-        servoController,
-        ledController
-    );
-
-    IStage* lineTracingBack = new LineTracing(
-        logger,
-        motorController,
-        lineSensor,
-        ledController
-    );
-
-    IStage* firstTurningTimed = new Turning(
+ReverseMotion* Injection::getNewReverseMotion()
+{
+    return new ReverseMotion(
         logger,
         motorController,
         ledController
     );
+}
 
-    IStage* secondTurningTimed = new Turning(
+Once* Injection::getNewOnce(unsigned long suppressTime = 0)
+{
+    return new Once(
         logger,
-        motorController,
-        ledController
+        suppressTime
     );
+}
 
-    IStage* turningBack = new Turning(
+Timed* Injection::getNewTimed(unsigned long suppressTime = 0)
+{
+    return new Timed(
         logger,
-        motorController,
-        ledController
+        suppressTime
     );
+}
 
-    lineTracing->stageTransition = detectBlock;
-    lineTracing->nextStage = turning;
-    turning->stageTransition = finishTurn;
-    turning->nextStage = lineTracingBack;
-    lineTracingBack->stageTransition = detectCross;
-    lineTracingBack->nextStage = firstTurningTimed;
-    firstTurningTimed->stageTransition = timedTurn;
-    firstTurningTimed->nextStage = turningBack;
-    turningBack->stageTransition = finishTurn;
-    turningBack->nextStage = lineTracing;
+DetectObstruction* Injection::getNewDetectObstruction(unsigned long suppressTime = 0)
+{
+    return new DetectObstruction(
+        logger,
+        suppressTime
+    );
+}
+
+DetectLine* Injection::getNewDetectLine(unsigned long suppressTime = 0)
+{
+    return new DetectLine(
+        logger,
+        suppressTime
+    );
+}
+
+DetectCross* Injection::getNewDetectCross(unsigned long suppressTime = 0)
+{
+    return new DetectCross(
+        logger,
+        suppressTime
+    );
 }
